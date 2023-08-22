efe63d96bcbdcddf10fe3f6f84cadba55c03959b
delaquash <olaide1191@gmail.com>
2023-08-22 14:22:28 +0100

    feat: add movie card component

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -4,10 +4,11 @@ import { COLORS, SPACING } from '../../theme/theme';
 // import { baseImagePath, nowPlayingMovies, popularMovies, upcomingMovies } from '../api/apicalls';
 import axios from 'axios';
 import { Dimensions, ScrollView, StatusBar } from 'react-native';
-import { nowPlayingMovies, popularMovies, upcomingMovies } from '../api/apicalls';
+import { baseImagePath, nowPlayingMovies, popularMovies, upcomingMovies } from '../api/apicalls';
 import CategoryHeader from '../components/Categoryheader';
 import InputHeader from '../components/InputHeader';
 import SubMovieCard from '../components/SubMovieCard';
+import MovieCard from '../components/MovieCard';
 
 const { height, width } = Dimensions.get("window")
 const getNowPlayingList = async () => {
@@ -95,19 +96,74 @@ const HomeScreen = ({navigation}: any) => {
         <View style={styles.inputContainerHeader}>
           <InputHeader />
         </View>
-        <CategoryHeader title={'Now Playing'} />
-        <CategoryHeader title={'Upcoming'} />
+        <CategoryHeader 
+          title={'Now Playing'} 
+        />
+        <FlatList
+          horizontal
+          keyExtractor={(item:any)=>item.id}
+          contentContainerStyle={styles.containerGap}
+          data={nowPlayingMovieList}
+          renderItem={({item, index})=>
+            <MovieCard
+              shouldMarginatedAtEnd={true}
+              cardFunction={()=> (
+                navigation.push("Movie Details", {movieid: item.id})
+              )}
+              cardWidth= {width * 0.7 }
+              isFirst= {index == 0 ? true : false}
+              isLast ={index == upcomingMovies?.length - 1 ? true : false }
+              title={item.original_title}
+              imagePath={baseImagePath("w780", item.poster_path)}
+              genre={item.genre_ids.slice(1, 4)}
+              vote_average={item.vote_average}
+              vote_count={item.vote_count}
+            /> 
+          }
+        />
+        <CategoryHeader 
+          title={'Upcoming'} 
+        />
+        <FlatList
+          horizontal
+          keyExtractor={(item:any)=>item.id}
+          contentContainerStyle={styles.containerGap}
+          data={popularMovies}
+          renderItem={({item, index})=>
+            <SubMovieCard
+              shouldMarginatedAtEnd={true}
+              cardFunction={()=> (
+                navigation.push("Movie Details", {movieid: item.id})
+              )}
+              cardWidth= {width / 3 }
+              isFirst= {index == 0 ? true : false}
+              isLast ={index == upcomingMovies?.length - 1 ? true : false }
+              title={item.original_title}
+              imagePath={baseImagePath("w342", item.poster_path)}
+            /> 
+          }
+          
+        />
         <CategoryHeader title={'Popular'} />
         <FlatList
+          horizontal
           keyExtractor={(item:any)=>item.id}
           contentContainerStyle={styles.containerGap}
           data={upComingMoviesList}
           renderItem={({item, index})=>
-            <SubMovieCard 
+            <SubMovieCard
+              shouldMarginatedAtEnd={true}
+              cardFunction={()=> (
+                navigation.push("Movie Details", {movieid: item.id})
+              )}
+              cardWidth= {width / 3 }
+              isFirst= {index == 0 ? true : false}
+              isLast ={index == upcomingMovies?.length - 1 ? true : false }
               title={item.original_title}
+              imagePath={baseImagePath("w342", item.poster_path)}
             /> 
           }
-          horizontal
+          
         />
     </ScrollView>
   )
