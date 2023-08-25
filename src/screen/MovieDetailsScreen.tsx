99bc2bc1311124386bec1c2fa82c2349d1d91d83
delaquash <olaide1191@gmail.com>
2023-08-25 11:16:09 +0100

    feat: add appheader component

--- src/screen/MovieDetailsScreen.tsx
+++ src/screen/MovieDetailsScreen.tsx
@@ -1,5 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+  StatusBar,
+  ImageBackground,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import {baseImagePath, movieCastDetails, movieDetails} from '../api/apicalls';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../../theme/theme';
 
 const MovieDetailsScreen = () => {
   return (
