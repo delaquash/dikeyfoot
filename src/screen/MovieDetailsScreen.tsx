81d2a4dd3186368007b8d834edfff34fe4a2a21d
delaquash <olaide1191@gmail.com>
2023-08-28 09:53:51 +0100

    fixed: movie detail screen

--- src/screen/MovieDetailsScreen.tsx
+++ src/screen/MovieDetailsScreen.tsx
@@ -20,6 +20,10 @@ import {
   SPACING,
 } from '../../theme/theme';
 import AppHeader from '../components/AppHeaser';
+import { LinearGradient } from "expo-linear-gradient";
+import CustomIcon from '../components/CustomIcon';
+import CategoryHeader from '../components/Categoryheader';
+import CastCard from '../components/CastCard';
 
 const getMovieDetails = async (movieid: number) => {
   try {
@@ -87,8 +91,109 @@ const MovieDetailsScreen = ({ navigation, route }: any) => {
   }
 
   return (
-    <ScrollView>
-      
+    <ScrollView
+      style={styles.container}
+      bounces={false}
+      showsVerticalScrollIndicator={false}>
+      <StatusBar hidden />
+
+      <View>
+        <ImageBackground
+          source={{
+            uri: baseImagePath('w780', movieData?.backdrop_path),
+          }}
+          style={styles.imageBG}>
+          <LinearGradient
+            colors={[COLORS.BlackRGB10, COLORS.Black]}
+            style={styles.linearGradient}>
+            <View style={styles.appHeaderContainer}>
+              <AppHeader
+                name="close"
+                header={''}
+                action={() => navigation.goBack()}
+              />
+            </View>
+          </LinearGradient>
+        </ImageBackground>
+        <View style={styles.imageBG}></View>
+        <Image
+          source={{uri: baseImagePath('w342', movieData?.poster_path)}}
+          style={styles.cardImage}
+        />
+      </View>
+
+      <View style={styles.timeContainer}>
+        <CustomIcon name="clock" style={styles.clockIcon} />
+        <Text style={styles.runtimeText}>
+          {Math.floor(movieData?.runtime / 60)}h{' '}
+          {Math.floor(movieData?.runtime % 60)}m
+        </Text>
+      </View>
+
+      <View>
+        <Text style={styles.title}>{movieData?.original_title}</Text>
+        <View style={styles.genreContainer}>
+          {movieData?.genres.map((item: any) => {
+            return (
+              <View style={styles.genreBox} key={item.id}>
+                <Text style={styles.genreText}>{item.name}</Text>
+              </View>
+            );
+          })}
+        </View>
+        <Text style={styles.tagline}>{movieData?.tagline}</Text>
+      </View>
+
+      <View style={styles.infoContainer}>
+        <View style={styles.rateContainer}>
+          <CustomIcon name="star" style={styles.starIcon} />
+          <Text style={styles.runtimeText}>
+            {movieData?.vote_average.toFixed(1)} ({movieData?.vote_count})
+          </Text>
+          <Text style={styles.runtimeText}>
+            {movieData?.release_date.substring(8, 10)}{' '}
+            {new Date(movieData?.release_date).toLocaleString('default', {
+              month: 'long',
+            })}{' '}
+            {movieData?.release_date.substring(0, 4)}
+          </Text>
+        </View>
+        <Text style={styles.descriptionText}>{movieData?.overview}</Text>
+      </View>
+
+      <View>
+        <CategoryHeader title="Top Cast" />
+        <FlatList
+          data={movieCastData}
+          keyExtractor={(item: any) => item.id}
+          horizontal
+          contentContainerStyle={styles.containerGap24}
+          renderItem={({item, index}) => (
+            <CastCard
+              shouldMarginatedAtEnd={true}
+              cardWidth={80}
+              isFirst={index == 0 ? true : false}
+              isLast={index == movieCastData?.length - 1 ? true : false}
+              imagePath={baseImagePath('w185', item.profile_path)}
+              title={item.original_name}
+              subtitle={item.character}
+            />
+          )}
+        />
+
+        <View>
+          <TouchableOpacity
+            style={styles.buttonBG}
+            onPress={() => {
+              navigation.push('SeatBooking', {
+                BgImage: baseImagePath('w780', movieData.backdrop_path),
+                PosterImage: baseImagePath('original', movieData.poster_path),
+              });
+            }}>
+            <Text style={styles.buttonText}>Select Seats</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </ScrollView>
   )
 }
