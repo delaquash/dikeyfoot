db34464eb9afe53218a09330b25cd7e292ee190f
delaquash <olaide1191@gmail.com>
2023-08-28 17:06:55 +0100

    fixed: generate days function

--- src/screen/SeatBookingScreen.tsx
+++ src/screen/SeatBookingScreen.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../../theme/theme';
 
 const timeArray: string[] = [
   '10:30',
@@ -10,13 +11,11 @@ const timeArray: string[] = [
   '21:00',
 ];
 
-const generateDate = () => {
+const generateDate = ():string[] => {
   const date = new Date();
   let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   let weekdays = [];
-  /* The line `for(i=0, i<7, i++)` is attempting to create a for loop that iterates from 0 to 6
-  (inclusive). However, there is a syntax error in the for loop declaration. The correct syntax for
-  a for loop in JavaScript/TypeScript is `for (initialization; condition; increment)`. */
+
   for(let i=0; i<7; i++){
     let tempDate = {
       /* The code `date: new Date(date.getTime() + i * 24 * 60 * 60 * 1000).getDate()` is creating a
@@ -31,7 +30,13 @@ const generateDate = () => {
   return weekdays;
 }
 
-const SeatBookingScreen = () => {
+const SeatBookingScreen = ({navigation, route}: any) => {
+  const [dateArray, setDateArray] = useState<any[]>(generateDate());
+  const [selectedDateIndex, setSelectedDateIndex] = useState<any>();
+  const [price, setPrice] = useState<number>(0);
+  // const [twoDSeatArray, setTwoDSeatArray] = useState<any[][]>(generateSeats());
+  const [selectedSeatArray, setSelectedSeatArray] = useState([]);
+  const [selectedTimeIndex, setSelectedTimeIndex] = useState<any>();
   return (
     <View>
       <Text>SeatBookingScreen</Text>
@@ -41,4 +46,132 @@ const SeatBookingScreen = () => {
 
 export default SeatBookingScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
+  ImageBG: {
+    width: '100%',
+    aspectRatio: 3072 / 1727,
+  },
+  linearGradient: {
+    height: '100%',
+  },
+  appHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_20 * 2,
+  },
+  screenText: {
+    textAlign: 'center',
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.WhiteRGBA15,
+  },
+  seatContainer: {
+    marginVertical: SPACING.space_20,
+  },
+  containerGap20: {
+    gap: SPACING.space_20,
+  },
+  seatRow: {
+    flexDirection: 'row',
+    gap: SPACING.space_20,
+    justifyContent: 'center',
+  },
+  seatIcon: {
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+  },
+  seatRadioContainer: {
+    flexDirection: 'row',
+    marginTop: SPACING.space_36,
+    marginBottom: SPACING.space_10,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  radioContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_10,
+    alignItems: 'center',
+  },
+  radioIcon: {
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.White,
+  },
+  radioText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.White,
+  },
+  containerGap24: {
+    gap: SPACING.space_24,
+  },
+  dateContainer: {
+    width: SPACING.space_10 * 7,
+    height: SPACING.space_10 * 10,
+    borderRadius: SPACING.space_10 * 10,
+    backgroundColor: COLORS.DarkGrey,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dateText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+  },
+  dayText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.White,
+  },
+  OutterContainer: {
+    marginVertical: SPACING.space_24,
+  },
+  timeContainer: {
+    paddingVertical: SPACING.space_10,
+    borderWidth: 1,
+    borderColor: COLORS.WhiteRGBA50,
+    paddingHorizontal: SPACING.space_20,
+    borderRadius: BORDERRADIUS.radius_25,
+    backgroundColor: COLORS.DarkGrey,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  timeText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  buttonPriceContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: SPACING.space_24,
+    paddingBottom: SPACING.space_24,
+  },
+  priceContainer: {
+    alignItems: 'center',
+  },
+  totalPriceText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.Grey,
+  },
+  price: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+  },
+  buttonText: {
+    borderRadius: BORDERRADIUS.radius_25,
+    paddingHorizontal: SPACING.space_24,
+    paddingVertical: SPACING.space_10,
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.White,
+    backgroundColor: COLORS.Orange,
+  },
+});
+
