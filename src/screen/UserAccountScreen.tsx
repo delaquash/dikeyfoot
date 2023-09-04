e289c174e2c3b587a0a6182f5e231303e655e2b9
delaquash <olaide1191@gmail.com>
2023-09-04 16:06:24 +0100

    feat: completed userprofile screen

--- src/screen/UserAccountScreen.tsx
+++ src/screen/UserAccountScreen.tsx
@@ -1,14 +1,84 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import SettingComponent from '../components/Settings';
+import AppHeader from '../components/AppHeaser';
+import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../../theme/theme';
 
-const UserAccountScreen = () => {
+const UserAccountScreen = ({navigation}: any) => {
   return (
-    <View>
-      <Text>UserAccountScreen</Text>
-    </View>
-  )
+  <View style={styles.container}>
+  <StatusBar hidden />
+  <View style={styles.appHeaderContainer}>
+    <AppHeader
+      name="close"
+      header={'My Profile'}
+      action={() => navigation.goBack()}
+    />
+  </View>
+
+  <View style={styles.profileContainer}>
+    <Image
+      source={require('../assets/image/Emmanuel.jpg')}
+      style={styles.avatarImage}
+    />
+    <Text style={styles.avatarText}>Olaide Emmanuel</Text>
+  </View>
+
+  <View style={styles.profileContainer}>
+    <SettingComponent
+      icon="user"
+      heading="Account"
+      subheading="Edit Profile"
+      subtitle="Change Password"
+    />
+    <SettingComponent
+      icon="setting"
+      heading="Settings"
+      subheading="Theme"
+      subtitle="Permissions"
+    />
+    <SettingComponent
+      icon="dollar"
+      heading="Offers & Refferrals"
+      subheading="Offer"
+      subtitle="Refferrals"
+    />
+    <SettingComponent
+      icon="info"
+      heading="About"
+      subheading="About Movies"
+      subtitle="more"
+    />
+  </View>
+</View>
+);
 }
 
 export default UserAccountScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
+  appHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_20 * 2,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    padding: SPACING.space_36,
+  },
+  avatarImage: {
+    height: 80,
+    width: 80,
+    borderRadius: 80,
+  },
+  avatarText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_16,
+    marginTop: SPACING.space_16,
+    color: COLORS.White,
+  },
+});
